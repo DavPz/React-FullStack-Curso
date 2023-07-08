0327ccfdadc58af87c9e4d0ee5c2b9bb95d883a5
David.Pugliese <davplzt@gmail.com>
2023-07-07 21:48:37 -0500

    Data Layout

---
+++ 5-cart-app/src/data/productos.js
@@ -0,0 +1,38 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Teclado Mecanico RGB',
+        description: 'Teclado mecanico con luces RGB y swithces cherry re',
+        price: 300,
+    },
+    {
+        id: 2,
+        name: 'Samgsung Smart TV 55',
+        description: 'Televisor LED 55 Pulgadas',
+        price: 1000,
+    },
+    {
+        id: 3,
+        name: 'Audifono Bluetooth Sony',
+        description: 'Audifono Sony Bluetooth con cancelacion de ruido',
+        price: 150,
+    },
+    {
+        id: 4,
+        name: 'Memoria Corsair 8GB DDR',
+        description: 'Memoria Ram Corsair de alto rendimiento',
+        price: 300,
+    },
+    {
+        id: 5,
+        name: 'Asus Nvidia RTX 4090',
+        description: 'Tarjeta Grafica Asus RTX 4090',
+        price: 1250,
+    },
+    {
+        id: 6,
+        name: 'CPU Intel Core I5',
+        description: 'Procesador Core I5',
+        price: 450,
+    },
+]
