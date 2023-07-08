0313fc883d88b90a72a2f8b4f3e1c3352c55e79f
David.Pugliese <davplzt@gmail.com>
2023-07-07 21:12:20 -0500

    product layout

--- 5-cart-app/src/CartApp.jsx
+++ 5-cart-app/src/CartApp.jsx
@@ -1,7 +1,77 @@
 export const CartApp = () => {
-    return(
+    return (
         <>
-        <h3>Cart App</h3>
+
+            <div className="container">
+
+                <h3>Cart App</h3>
+
+                <div className="row">
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Teclado Mecanico RGB</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 1000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Samgsung Smart TV 55</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 3000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Audifono Bluetooth Sony</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 750</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Memoria Corsair 8GB DDR</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 500</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Asus Nvidia RTX</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 5000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-4 my-2">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">CPU Intel Core I5</h5>
+                                <p className="card-text">Teclado mecanico con luces RGB y swithces cherry re</p>
+                                <p className="card-text">$ 3000</p>
+                                <button className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
         </>
     );
 }
