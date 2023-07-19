26b88a64db4aebc1ef69e4294765afd2bf8b9033
David.Pugliese <davplzt@gmail.com>
2023-07-18 21:15:51 -0500

    creating customized hook (useAuth) and arranging code

--- 6-users-app/src/UsersApp.jsx
+++ 6-users-app/src/UsersApp.jsx
@@ -4,42 +4,12 @@ import { UsersPages } from "./pages/UsersPages";
 import { loginReducer } from "./auth/reducers/loginReducer";
 import Swal from "sweetalert2";
 import { Navbar } from "./components/layout/Navbar";
+import { useAuth } from "./auth/hooks/useAuth";
 
-const initialLogin = JSON.parse(sessionStorage.getItem('login')) || {
-    isAuth: false,
-    user: undefined,
-}
+
 export const UsersApp = () => {
 
-    const [login, dispatch] = useReducer(loginReducer, initialLogin);
-
-    const handlerLogin = ({ userName, password }) => {
-
-        if (userName === 'admin' && password === '12345') {
-            const user = { userName: 'admin' };
-            dispatch({
-                type: 'login',
-                payload: user,
-            });
-            sessionStorage.setItem('login', JSON.stringify({
-                isAuth: true,
-                user,
-            }));
-
-        } else {
-            Swal.fire('Error de Login', 'Username y/o Password incorrectos', 'error');
-        }
-
-    }
-
-    const handlerLogout = () => {
-        dispatch({
-            type: 'logout',
-        });
-
-        sessionStorage.removeItem('login');
-
-    }
+    const { login, handlerLogin, handlerLogout } = useAuth();
 
     return (
         <>
