b30cbd4a5a5f1f6551aa6f461f123277344292da
David.Pugliese <davplzt@gmail.com>
2023-07-26 23:09:53 -0500

    Delete Method

--- Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
+++ Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
@@ -5,16 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.usersapp.backendusersapp.models.entities.User;
@@ -28,30 +26,30 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public List<User> list(){
+    public List<User> list() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> show(@PathVariable Long id){
+    public ResponseEntity<?> show(@PathVariable Long id) {
         Optional<User> userOptional = userService.findById(id);
 
-        if(userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             return ResponseEntity.ok(userOptional.orElseThrow());
-        } 
-        
+        }
+
         return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> creatUser(@RequestBody User user){
+    public ResponseEntity<?> creatUser(@RequestBody User user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Long id){
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Long id) {
         Optional<User> o = userService.findById(id);
-        if(o.isPresent()){
+        if (o.isPresent()) {
             User userDb = o.get();
             userDb.setUsername(user.getUsername());
             userDb.setEmail(user.getEmail());
@@ -61,5 +59,15 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeUser(@PathVariable Long id) {
+        Optional<User> o = userService.findById(id);
+        if (o.isPresent()) {
+            userService.remove(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
