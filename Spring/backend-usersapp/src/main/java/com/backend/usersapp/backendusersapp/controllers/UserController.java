f1de1c3c299474e8393c4d199c3360d749b43a8f
David.Pugliese <davplzt@gmail.com>
2023-07-26 23:01:35 -0500

    PUT method

--- Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
+++ Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -47,6 +48,18 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Long id){
+        Optional<User> o = userService.findById(id);
+        if(o.isPresent()){
+            User userDb = o.get();
+            userDb.setUsername(user.getUsername());
+            userDb.setEmail(user.getEmail());
+            return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userDb));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
 
     
 }
