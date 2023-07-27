404a16a7e8f59d25b8fe652213a91879cb48c162
David.Pugliese <davplzt@gmail.com>
2023-07-26 22:52:41 -0500

    post method on controller

--- Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
+++ Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/controllers/UserController.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.usersapp.backendusersapp.models.entities.User;
@@ -34,6 +40,11 @@ public class UserController {
         } 
         
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> creatUser(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
 
 
