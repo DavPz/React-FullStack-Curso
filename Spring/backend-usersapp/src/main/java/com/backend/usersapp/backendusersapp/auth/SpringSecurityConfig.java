8bb999615d14db92b6fc825a74b23aacee536bfa
David.Pugliese <davplzt@gmail.com>
2023-07-31 22:00:38 -0500

    adding filter to the chain

--- Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
+++ Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/auth/SpringSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.backend.usersapp.backendusersapp.auth;
 
+import com.backend.usersapp.backendusersapp.auth.filters.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,11 +13,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfig {
 
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(authRules -> authRules
                         .requestMatchers(HttpMethod.GET, "/users").permitAll()
                         .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
