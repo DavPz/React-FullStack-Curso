d56e39fc01d9af85b867fec109cdbbd1f1007656
David.Pugliese <davplzt@gmail.com>
2023-08-02 00:10:09 -0500

    maven dependencies for JWT and implementing key jwt

--- Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/auth/TokenConfig.java
+++ Spring/backend-usersapp/src/main/java/com/backend/usersapp/backendusersapp/auth/TokenConfig.java
@@ -1,8 +1,14 @@
 package com.backend.usersapp.backendusersapp.auth;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
 public class TokenConfig {
 
-    public final static String SECRET_KEY = "algun_token_con_mi_secret";
+    //public final static String SECRET_KEY = "algun_token_con_mi_secret";
+    public final static Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     public final static String PREFIX_TOKEN = "Bearer ";
     public final static String HEADER_AUTHORIZATION = "Authorization";
 
