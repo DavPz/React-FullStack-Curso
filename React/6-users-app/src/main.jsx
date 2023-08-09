324a6f90553c83c98a243a46faf5ccc1c3065ff2
David.Pugliese <davplzt@gmail.com>
2023-08-08 22:31:51 -0500

    changing architecture for redux

--- React/6-users-app/src/main.jsx
+++ React/6-users-app/src/main.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { UsersApp } from './UsersApp.jsx'
 import './styles.css'
-import { LoginPage } from './auth/pages/LoginPage.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/context/AuthProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <UsersApp />
-      </AuthProvider>
+      <UsersApp />
     </BrowserRouter>
   </React.StrictMode>,
 )
--- React/6-users-app/src/routes/UserRoutes.jsx
+++ React/6-users-app/src/routes/UserRoutes.jsx
@@ -13,25 +13,23 @@ export const UserRoutes = () => {
 
     return (
         <>
-            <UserProvider>
-                <Navbar />
-                <Routes>
-                    <Route path="users" element={<UsersPages />} />
-                    {!login.isAdmin ||
-                        <>
-                            <Route path="users/register" element={
-                                <RegisterPage />
-                            } />
+            <Navbar />
+            <Routes>
+                <Route path="users" element={<UsersPages />} />
+                {!login.isAdmin ||
+                    <>
+                        <Route path="users/register" element={
+                            <RegisterPage />
+                        } />
 
-                            <Route path="users/edit/:id" element={
-                                <RegisterPage />
-                            } />
+                        <Route path="users/edit/:id" element={
+                            <RegisterPage />
+                        } />
 
-                        </>
-                    }
-                    <Route path="/" element={<Navigate to="/users" />} />
-                </Routes>
-            </UserProvider>
+                    </>
+                }
+                <Route path="/" element={<Navigate to="/users" />} />
+            </Routes>
         </>
     )
 }
